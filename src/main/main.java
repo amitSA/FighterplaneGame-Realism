cafa2e94ad10c9a5481cebe18d33078171c74c86
amitSA <send.amit@yahoo.com>
2014-06-12 22:43:39 -0700

    added header comments

---
+++ src/main/main.java
@@ -0,0 +1,34 @@
+/* APCS Dog Fight Game
+ * Copywrite 2014 Amit Saxena 
+ * 
+ * 
+ */
+
+
+
+package main;
+import javax.swing.JFrame;
+
+import otherstuff.GamePanel;
+import otherstuff.KeyInteraction;
+
+
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		 JFrame w = new JFrame("Simple Window");
+		    w.setBounds(10, 25, 1550, 800);
+		    w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		    GamePanel gamePanel = new GamePanel();
+		    
+		    KeyInteraction keyInteraction = new KeyInteraction(gamePanel,5);
+		
+		    w.addKeyListener(keyInteraction);
+		    w.add(gamePanel);
+		    w.setResizable(false);
+		    w.setVisible(true);
+	}
+}
